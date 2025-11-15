b27b03de7a1b000aa9aa86fb274e681a0d0f52b3
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-11-15 17:24:51 -0600

    added the import in app.ts file as well as wrote  the endpoints in it

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
-
+import reciperoutes from "./api/v1/routes/reciperoutes";
 
 const app = express();
 app.use(express.json());
+app.use("/api/v1/recipes", reciperoutes);
 
 export default app;
