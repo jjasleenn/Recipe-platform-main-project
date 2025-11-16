d3e0d74393791b117c8bb1fd87725c550a0e00d6
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-11-15 20:40:47 -0600

    added the import in the app.ts file for scheduler

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import reciperoutes from "./api/v1/routes/reciperoutes";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./api/v1/swagger/swagger";
+import { startScheduler } from "./api/v1/utils/scheduler";
 
 const app = express();
 app.use(express.json());
 app.use("/api/v1/recipes", reciperoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+startScheduler();
 
 export default app;
